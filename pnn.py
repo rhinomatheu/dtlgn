96e82b3f8ffdfde92f4354d3373cf9d7aa0f35d8
ryan <rmatheu@umd.edu>
2025-10-08 17:15:52 -0400

    balls

--- pnn.py
+++ pnn.py
@@ -1,5 +1,6 @@
 import jax
 import jax.numpy as jnp
+from jax.nn import relu
 import jax.random
 
 
@@ -17,13 +18,31 @@ def Linear(
     weight_count: int = out_features # biases
     weight_count += out_features * in_features # linear
 
-    weights = jax.random.normal(key, shape=(weight_count,)) * 0.1
+    _weights = jax.random.normal(key, shape=(weight_count,)) * 0.1
 
     if batched:
-        pass
+        @jax.jit
+        def linear_impl(weights, x):
+            biases = weights[:out_features]
+            W = jnp.reshape(weights[out_features:out_features+out_features*in_features], (out_features, in_features))
 
+            out = biases + jnp.einsum("ij,bj->bi", W, x)
+
+            return out
+        
+        return _weights, linear_impl
+    
     else:
-        pass
+        @jax.jit
+        def linear_impl(weights, x):
+            biases = weights[:out_features]
+            W = jnp.reshape(weights[out_features:out_features+out_features*in_features], (out_features, in_features))
+
+            out = biases + jnp.matmul(W, x)
+
+            return out
+        
+        return _weights, linear_impl
 
 def MultiLinear(
         in_features: int,
@@ -41,39 +60,39 @@ def MultiLinear(
     trig = in_features * (in_features - 1)//2
     weight_count += out_features * trig # multilinear
 
-    weights = jax.random.normal(key, shape=(weight_count,)) * 0.1
+    _weights = jax.random.normal(key, shape=(weight_count,)) * 0.1
 
     if batched:
         @jax.jit
-        def multilinear_impl(weights, input):
+        def multilinear_impl(weights, x):
             biases = weights[:out_features]
-            w = jnp.reshape(weights[out_features:out_features+in_features*out_features], (out_features, in_features))
+            w = jnp.reshape(weights[out_features:out_features+out_features*in_features], (out_features, in_features))
             W = jnp.zeros((out_features, in_features, in_features))
             idx = jnp.triu_indices(in_features, k=1)
             mask = (jnp.repeat(jnp.arange(out_features), trig), jnp.tile(idx[0], out_features), jnp.tile(idx[1], out_features))
             W = W.at[mask].set(weights[out_features+in_features*out_features:])
 
-            out = biases + jnp.einsum("ij,bj->bi", w, input) + jnp.einsum("bi,mij,bj->bm", input, W, input)
+            out = biases + jnp.einsum("ij,bj->bi", w, x) + jnp.einsum("bi,mij,bj->bm", x, W, x)
 
             return out
         
-        return weights, multilinear_impl
+        return _weights, multilinear_impl
     
     else:
         @jax.jit
-        def multilinear_impl(weights, input):
+        def multilinear_impl(weights, x):
             biases = weights[:out_features]
-            w = jnp.reshape(weights[out_features:out_features+in_features*out_features], (out_features, in_features))
+            w = jnp.reshape(weights[out_features:out_features+out_features*in_features], (out_features, in_features))
             W = jnp.zeros((out_features, in_features, in_features))
             idx = jnp.triu_indices(in_features, k=1)
             mask = (jnp.repeat(jnp.arange(out_features), trig), jnp.tile(idx[0], out_features), jnp.tile(idx[1], out_features))
             W = W.at[mask].set(weights[out_features+in_features*out_features:])
 
-            out = biases + jnp.matmul(w, input) + jnp.einsum("i,mij,j->m", input, W, input)
+            out = biases + jnp.matmul(w, x) + jnp.einsum("i,mij,j->m", x, W, x)
 
             return out
         
-        return weights, multilinear_impl
+        return _weights, multilinear_impl
 
 def PNN(
         features: list[int] | tuple[int],
@@ -90,7 +109,7 @@ def PNN(
     key = jax.random.split(key, num=len(features)-1)
 
     if len(features) == 2:
-        return MultiLinear(*features, key[0])
+        return MultiLinear(*features, key=key[0], batched=batched)
 
     _weights = jnp.array([])
     _weight_lengths = []
@@ -112,11 +131,156 @@ def PNN(
             out = _layers[i](weights[strides[i-1]:strides[i]], out)
         
         out = _layers[-1](weights[-_weight_lengths[-1]:], out)
+
         return out
     
     return _weights, pnn_impl
 
+def LNN(
+        features: list[int] | tuple[int],
+        *,
+        key: jax.Array = None,
+        batched = True,
+    ):
+
+    assert len(features) >= 2
+
+    if key is None:
+        key = jax.random.key(0)
+
+    key = jax.random.split(key, num=len(features)-1)
+
+    if len(features) == 2:
+        return Linear(*features, key=key[0], batched=batched)
+    
+    _weights = jnp.array([])
+    _weight_lengths = []
+    _layers = []
+
+    for i in range(len(features)-1):
+        w, l = Linear(features[i], features[i+1], key=key[i], batched=batched)
+        _weights = jnp.append(_weights, w)
+        _weight_lengths.append(len(w))
+        _layers.append(l)
+
+    strides = [int(x) for x in jnp.cumsum(jnp.array(_weight_lengths))]
+
+    @jax.jit
+    def lnn_impl(weights, x):
+
+        out = _layers[0](weights[:_weight_lengths[0]], x)
+        out = relu(out)
+        for i in range(1, len(_layers)-1):
+            out = _layers[i](weights[strides[i-1]:strides[i]], out)
+            out = relu(out)
+        
+        out = _layers[-1](weights[-_weight_lengths[-1]:], out)
+
+        return out
+    
+    return _weights, lnn_impl
+
 #
+
+# 3 gate circuit
+data = [
+    #           A   B   C   D   Z
+    jnp.array([-1, -1, -1, -1, -1]),
+    jnp.array([-1, -1, -1,  1, -1]),
+    jnp.array([-1, -1,  1, -1, -1]),
+    jnp.array([-1, -1,  1,  1,  1]),
+
+    jnp.array([-1,  1, -1, -1,  1]),
+    jnp.array([-1,  1, -1,  1,  1]),
+    jnp.array([-1,  1,  1, -1,  1]),
+    jnp.array([-1,  1,  1,  1, -1]),
+
+    jnp.array([ 1, -1, -1, -1,  1]),
+    jnp.array([ 1, -1, -1,  1,  1]),
+    jnp.array([ 1, -1,  1, -1,  1]),
+    jnp.array([ 1, -1,  1,  1, -1]),
+
+    jnp.array([ 1,  1, -1, -1,  1]),
+    jnp.array([ 1,  1, -1,  1,  1]),
+    jnp.array([ 1,  1,  1, -1,  1]),
+    jnp.array([ 1,  1,  1,  1, -1]),
+]
+
+#
+
+def main3():
+
+    import random
+    key = jax.random.key(random.randint(0, 1000))
+    
+    w, nn = LNN(features=(4, 10, 5, 1), batched=False, key=key)
+
+    S = jnp.array(
+        [
+            [0, 0, 0, 0],
+            [0, 0, 0, 1],
+            [0, 0, 1, 0],
+            [0, 0, 1, 1],
+            [0, 1, 0, 0],
+            [0, 1, 0, 1],
+            [0, 1, 1, 0],
+            [0, 1, 1, 1],
+            [1, 0, 0, 0],
+            [1, 0, 0, 1],
+            [1, 0, 1, 0],
+            [1, 0, 1, 1],
+            [1, 1, 0, 0],
+            [1, 1, 0, 1],
+            [1, 1, 1, 0],
+            [1, 1, 1, 1],
+        ],
+        dtype=float
+    )
+
+    @jax.jit
+    def f(weights, x):
+        prod = 0.
+        for I in S:
+            prod += nn(weights, I)[0]*jnp.prod(jnp.pow(x, I))
+        return prod
+    
+    @jax.jit
+    def loss(weights, data):
+        l = 0.
+        for datum in data:
+            l += jnp.square(f(weights, datum[:-1]) - datum[-1])
+        return l
+    
+    grad_loss = jax.jit(jax.grad(loss))
+
+    lr = 0.001
+    N = 5000
+    epochs = 10
+
+    print("batching data")
+    batches = [tuple(random.sample(data, len(data))) for _ in range(epochs)]
+
+    print(f"preloss: {loss(w, data)}")
+
+    for i in range(N):
+        for batch in batches:
+
+            grad = grad_loss(w, batch)
+            w = w - lr*grad
+        
+        print(i)
+    
+    print(f"postloss: {loss(w, data)}")
+
+    # print(w)
+
+    print("   PNN     |   DATA    ")
+    print("-----------|-----------")
+    for datum in data:
+        out = f(w, datum[:-1])
+        print(f"    {out:>2.0f}     |    {datum[4]:>2d}     ")
+    
+
 
 def main2():
     import numpy as np
@@ -137,57 +301,13 @@ def main():
     import numpy as np
     import random
 
-    # 3 gate circuit
-    data = [
-    #     A   B   C   D   Z
-        jnp.array([-1, -1, -1, -1, -1]),
-        jnp.array([-1, -1, -1,  1, -1]),
-        jnp.array([-1, -1,  1, -1, -1]),
-        jnp.array([-1, -1,  1,  1,  1]),
-
-        jnp.array([-1,  1, -1, -1,  1]),
-        jnp.array([-1,  1, -1,  1,  1]),
-        jnp.array([-1,  1,  1, -1,  1]),
-        jnp.array([-1,  1,  1,  1, -1]),
-
-        jnp.array([ 1, -1, -1, -1,  1]),
-        jnp.array([ 1, -1, -1,  1,  1]),
-        jnp.array([ 1, -1,  1, -1,  1]),
-        jnp.array([ 1, -1,  1,  1, -1]),
-
-        jnp.array([ 1,  1, -1, -1,  1]),
-        jnp.array([ 1,  1, -1,  1,  1]),
-        jnp.array([ 1,  1,  1, -1,  1]),
-        jnp.array([ 1,  1,  1,  1, -1]),
-    ]
-
-    w1, layer1 = MultiLinear(4, 2)
-    w2, layer2 = MultiLinear(2, 1)
-
-    n1 = w1.size
-    n2 = w2.size
-
-    w = jnp.append(w1, w2)
-
-    @jax.jit
-    def forward(weights, x):
-        
-        ww1 = weights[:n1]
-        ww2 = weights[n1:]
-
-        z = layer1(ww1, x)
-        y = layer2(ww2, z)
-
-        return y
-    
-    x = jnp.array(np.random.rand(4))
-
     features = (4, 10, 5, 1)
-    print(f"jitting network with feature layers: {features}")
-
+    
+    print(f"jitting p network with feature layers: {features}")
     w, forward = PNN(features=features, batched=False)
 
-    print(forward(w, x))
+    print(f"jitting l network with feature layers: {features}")
+    w2, forward2 = LNN(features=features, batched=False)
 
     @jax.jit
     def loss(weights, data):
@@ -196,14 +316,27 @@ def main():
             l += jnp.square(forward(weights, datum[:-1]) - datum[-1])
         return l[0]
     
-    print(loss(w, data))
+    @jax.jit
+    def loss2(weights, data):
+        l = 0
+        for datum in data:
+            l += jnp.square(forward2(weights, datum[:-1]) - datum[-1])
+        return l[0]
+    
+
     # jit that shit
+
+    print("jitting grad loss")
     grad_loss = jax.jit(jax.grad(loss))
+
+    print("jitting grad loss2")
+    grad_loss2 = jax.jit(jax.grad(loss2))
 
     lr = 0.001
     N = 500
     epochs = 10
 
+    print("batching data")
     batches = [tuple(random.sample(data, len(data))) for _ in range(epochs)]
 
 
@@ -211,19 +344,20 @@ def main():
         for batch in batches:
 
             grad = grad_loss(w, batch)
+            grad2 = grad_loss2(w2, batch)
             w = w - lr*grad
+            w2 = w2 - lr*grad2
         
         print(i)
 
-    print(w)
-    print(loss(w, data))
-
-    print("  LEARNED  |   DATA     ")
-    print("-----------|------------")
+    print("   PNN     |   DATA    |    LNN    ")
+    print("-----------|-----------|-----------")
     for datum in data:
         out = forward(w, datum[:-1])[0]
-        print(f"    {out:>2.0f}     |  {datum[4]:>2d}   ")
+        out2 = forward2(w2, datum[:-1])[0]
+        print(f"    {out:>2.0f}     |    {datum[4]:>2d}     |    {out2:>2.0f}     ")
 
 if __name__ == "__main__":
-    main()
+    # main()
     # main2()
+    main3()
